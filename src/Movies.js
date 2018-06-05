b56b278f45a5bfb575e4f08bb424ef288acb7c25
bhaveshc20 <bhaveshc789@gmail.com>
2018-06-05 16:32:16 -0700

    posters view working

--- src/Movies.js
+++ src/Movies.js
@@ -2,19 +2,39 @@ import React from 'react';
 import {
   ScrollView,
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
 
-import { movies } from './data'
+import MoviePoster from './MoviePoster';
+import { movies } from './data';
 
 export default class Movies extends React.Component {
   render() {
     return (
-      <View>
-        <ScrollView>
-          {movies.map((movie, index) => <Text>{movie.title}</Text>)}
-          </ScrollView>
-        </View>
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          {movies.map((movie, index) => <MoviePoster
+            movie={movie}
+            onOpen={this.openMovie}
+            key={index}
+          />)}
+        </ScrollView>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,         // start below status bar
+  },
+  scrollContent: {
+    flexDirection: 'row',   // arrange posters in rows
+    flexWrap: 'wrap',       // allow multiple rows
+  },
+});
